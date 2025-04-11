16484bc29359797b3bef0db028349247b7e290c9
Xpeditor15 <stanleyng.ws@gmail.com>
2025-04-11 17:32:54 +0800

    Created README for navigating the repository and some minor updates to the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
-#include <Arduino.h>
+#include "main.h"
 
 void setup() {
     Serial.begin(115200);
+
+    pinMode(RGB_BUILTIN, OUTPUT);
+
+    IrReceiver.begin(RECEIV_PIN, false);
+    Serial.println("IR receiver initialized");
+    disableLEDFeedback(); 
 }
 
 void loop() {
     Serial.println("Hello world");
+
+    if (IrReceiver.decode()) {
+        char protocol[20];
+        Serial.println("Successfully decoded IR signals");
+    }
+
     delay(1000);
 }
---
+++ src/main.h
@@ -0,0 +1,18 @@
+/*
+This header file is used for including dependencies for the project and function declarations
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
+
+#include <IRremote.hpp>
+
+#define RECEIV_PIN 15
+#define TRANS_PIN 40
+
+#endif
