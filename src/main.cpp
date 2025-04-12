da4aa658ab36884b62171879c83d82a7edd550e0
Xpeditor15 <stanleyng.ws@gmail.com>
2025-04-12 11:05:00 +0800

    Edited README file and made some other changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include "main.h"
 
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(115200); 
 
     pinMode(RGB_BUILTIN, OUTPUT);
+    pinMode(TFT_LED, OUTPUT);
+    pinMode(Button, INPUT_PULLUP); //sets the RGB light and LCD backlight as output, button as input pullup
+    
+    digitalWrite(TFT_LED, HIGH); //sets the LCD backlight as high so the screen lights up
+    
+    tft.initR(INITR_144GREENTAB);
 
-    IrReceiver.begin(RECEIV_PIN, false);
+    tft.setFont();
+    tft.fillScreen()
+
+    IrReceiver.begin(RECEIV_PIN, false); //initializes the IR receiver
     Serial.println("IR receiver initialized");
-    disableLEDFeedback(); 
+    IrSender.begin(TRANS_PIN); //initializes the IR transmitter
+    Serial.println("IR transmitter initialized"); 
+    disableLEDFeedback();
 }
 
 void loop() {
-    Serial.println("Hello world");
-
     if (IrReceiver.decode()) {
         char protocol[20];
         Serial.println("Successfully decoded IR signals");
--- src/main.h
+++ src/main.h
@@ -1,5 +1,5 @@
 /*
-This header file is used for including dependencies for the project and function declarations
+Header file used for including dependencies for the project and function declarations
 */
 
 #ifndef MAIN_H
@@ -12,7 +12,16 @@ This header file is used for including dependencies for the project and function
 
 #include <IRremote.hpp>
 
-#define RECEIV_PIN 15
+//definitions for IR receiver and transmitter data pins
+#define RECEIV_PIN 15 //
 #define TRANS_PIN 40
+
+//definitions for LCD screen pins
+#define TFT_CS 10
+#define TFT_DC 8
+#define TFT_RST 9
+#define TFT_LED 21
+
+#define Button 42 //button used to send IR signals
 
 #endif
